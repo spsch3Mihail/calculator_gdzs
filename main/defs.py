0ad52b27a24a03622d9fddf3062337775af50149
Mihail <vyazoff@bk.ru>
2023-06-23 13:22:14 +1000

    add closure.py

--- main/defs.py
+++ main/defs.py
@@ -1,4 +1,17 @@
 from datetime import timedelta
+import flet as ft
+
+
+def main(page: ft.Page):
+    t = ft.Text(value='Hello world!', color='green' )
+    page.controls.append(t)
+    page.update()
+
+
+ft.app(target=main, view=ft.WEB_BROWSER)
+
+
+
 
 
 def calculator():
