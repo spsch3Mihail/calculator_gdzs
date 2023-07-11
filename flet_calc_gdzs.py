91dc74bb72519108153cb78b946bddee398b32d6
Mihail <vyazoff@bk.ru>
2023-07-11 17:21:05 +1000

    app created.v1.0

---
+++ flet_calc_gdzs.py
@@ -0,0 +1,39 @@
+import flet as ft
+from datetime import timedelta
+
+
+def main(page):
+    def calculator(e):
+        press_min = int(min_press.value)
+        time_now = timedelta(hours=int(hours.value), minutes=int(minutes.value))
+        max_press_drop = round(press_min / 3)
+        press_ctrl_exit = press_min - max_press_drop
+        delta_time = timedelta(minutes=(round(max_press_drop * 6.8 / 45)))
+        total_time = timedelta(minutes=(round(press_min * 6.8 / 45)))
+        time_on_exit = time_now + delta_time
+        comeback_time = time_now + total_time
+        d.value = f' Pmax.пад = {max_press_drop};  Рк.вых =  {press_ctrl_exit};  Дельта Т = {delta_time};  Тобщ = {total_time};  Т вых = {time_on_exit};  Т возвр = {comeback_time}'
+
+        page.update()
+
+
+    min_press = ft.TextField(label='введите минимальное давление:', width=280, helper_text='Введите время включения в НДС:')
+    hours = ft.TextField(label='Введите часы:', width=280, hint_text='от 0 до 24')
+    minutes = ft.TextField(label='Введите минуты:', width=280,hint_text='от 0 до 60')
+
+
+
+    d = ft.TextField(label='Результат')
+    c = ft.ElevatedButton(text='Рассчитать', on_click=calculator,
+                          bgcolor=ft.colors.BLACK, color=ft.colors.WHITE)
+
+    page.controls.append(ft.Text('Калькулятор ГДЗС v1.0',style=ft.TextThemeStyle.DISPLAY_SMALL))
+    # page.controls.pop()
+    page.update()
+    page.add(ft.Column([min_press, hours, minutes, c, d]))
+
+
+
+ft.app(target=main)
+
+
---
+++ flet_calc_gdzs.spec
@@ -0,0 +1,46 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+block_cipher = None
+
+
+a = Analysis(
+    ['flet_calc_gdzs.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False,
+)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    [],
+    name='flet_calc_gdzs',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    version='C:\\Users\\User\\AppData\\Local\\Temp\\2d02ed3f-0763-47ce-974a-3594a3a2b2eb',
+    icon=['cacl.png'],
+)
